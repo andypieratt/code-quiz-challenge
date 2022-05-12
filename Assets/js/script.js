6f19b3af79a6ab616b354af269da16797e4b0ce4
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-11 21:57:40 -0600

    added some more functions within js for highscores, still trying to figure out issues between switching to questions and storing the data for the score list

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,35 @@
-var startBtn = $("#start-button");
+// Button Variables
+var startBtn = $("#start-btn");
 var nextBtn = $("#next-btn");
-var questionOne = $("#question-1", questionOneBegin());
-var questionTwo = $("#question-2");
+var wrongBtn = $("#wrong");
+var correctBtn = $("#correct");
+
+// Question and Choice Variables
+var questionOne = $("#question-1", questionOneTest());
+var questionTwo = $("#question-2", questionTwoTest());
 var questionThree = $("#question-3");
 var questionFour = $("#question-4");
 var questionFive = $("#question-5");
 var openScreen = $("#opening-screen");
-var userChoice = [];
+var userChoice = (correctBtn, wrongBtn);
+
+// var questionsArr = [
+//     { question1: "#question1" },
+//     { question2: "#question2" },
+//     { question3: "#question3" },
+//     { question4: "#question4" },
+//     { question5: "#question5" }
+// ]
 
 // Timer and Current Index #
 var timer = 30;
 var currentIndex = 0;
 
+// Highscore Variables
+var scoreList = $("high-scores");
 
-// Welcom Screen Function
+
+// Welcome Screen Function
 function openingScreen() {
     // Hide Questions 
     questionOne.hide();
@@ -21,13 +37,13 @@ function openingScreen() {
     questionThree.hide();
     questionFour.hide();
     questionFive.hide();
-
     // Hide Next Question Button
     nextBtn.hide();
     // Show Opening Screen
     openScreen.show();
 }
 
+// Opening Screen Call
 openingScreen();
 
 // Start Game Function
@@ -38,22 +54,86 @@ function startGame() {
     openScreen.hide();
     // Show Question One
     questionOne.show();
+    // Show Next Button
+    nextBtn.show();
     // Being Timer Countdown
 }
 
+// Start Game Call
 startBtn.on('click', startGame);
 
-
 // Question One Function
-function questionOneBegin() {
-    var correct = $("#correct1");
-    var wrong = $("#wrong1");
-    // Show Next Button
-    nextBtn.show();
-    if (userChoice === correct) {
+function questionOneTest() {
+    var correct = correctBtn;
+    var wrong = wrongBtn;
+    if (userChoice == correct) {
+        alert('button clicked');
         questionTwo.show();
-    } else if (userChoice === wrong) {
+    } else if (userChoice == wrong) {
         alert(Incorrect);
         questionTwo.show();  
     }
 } 
+
+// Question Two Function
+function questionTwoTest() {
+    var correct = correctBtn.on('click',nextQuestion());
+    var wrong = wrongBtn.on('click', nextQuestion());
+    if (userChoice === correct) {
+        questionThree.show();
+        nextBtn.on('click', questionTwo);
+    } else if (userChoice === wrong) {
+        alert(Incorrect);
+        questionThree.show();  
+    }
+}
+
+function nextQuestion() {
+    var correct = correctBtn.o;
+    var wrong = wrongBtn;
+    if (correctBtn === correct) {
+        currentIndex++;
+    } else if (wrong === wrongBtn) {
+        currentIndex++;
+    }
+}
+
+// END GAME
+function endGame() {
+    // Hide Next Button
+    nextBtn.hide();
+    // Show Start Button
+    startBtn.show();
+    //Alert Score
+    //Capture User Initials
+    //Save Score and Initials to Local Storage
+    // Re-render High Scores
+    renderScores();
+}
+
+// RENDER HIGH SCORES
+function renderScores() {
+    // Get Current Local Storage Data
+    var currentScores = JSON.parse(localStorage.getItem('highscore')) || [];
+    // Clear High Score Field
+    scoreList.empty();
+    // Loop Through Array of Score Objects
+    if (scoreList.length === 0) {
+        return scoreList.text('No Scores Yet!');
+    }
+    for (var i = 0; i < currentScores.length; i++) {
+        var scoreObj = currentScores[i];
+        var newLi = $('<li>', { class: 'list-item' })
+            .text(scoreObj, initials + '---------' + scoreObj.score);
+        scoreList.append(newLi);
+
+    }
+}
+
+// correctBtn.on('click', function () {
+//     currentIndex++;
+//     if (currentIndex === questionsArr.length) {
+        
+//     }
+// });
+
